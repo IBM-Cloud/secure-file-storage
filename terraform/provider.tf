8e53203d668339952036f01ae03c3b9d6aece523
Henrik Loeser <hloeser@de.ibm.com>
2020-12-14 08:11:24 +0100

    new TF provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "~>1.16"
+      version = "~>1.17"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
