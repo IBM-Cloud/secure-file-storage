1cd9fd06434017c9b53f3f7cd0a23936c610956b
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2022-05-03 15:50:57 -0400

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
+  #   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   ibmcloud_timeout = var.ibmcloud_timeout
 }
