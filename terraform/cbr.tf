27c111dd6dddfe5645099760671ad3f6e06fe995
Henrik Loeser <hloeser@de.ibm.com>
2022-12-15 08:03:03 +0100

    smoothing

--- terraform/cbr.tf
+++ terraform/cbr.tf
@@ -1,11 +1,11 @@
 # CBR objects are integrated as optional module
 module "cbr_objects" {
-  count            = var.deploy_cbr ? 1 : 0
-  source           = "./cbr"
-  vpcname          = var.vpcname
-  iks_cluster_name = var.iks_cluster_name
-  enforcement_mode = var.enforcement_mode
-  cos              = ibm_resource_instance.cos
-  keyprotect       = ibm_resource_instance.keyprotect
-  homezone_iprange = var.homezone_iprange
+  count                = var.deploy_cbr ? 1 : 0
+  source               = "./cbr"
+  vpcname              = var.vpcname
+  iks_cluster_name     = var.iks_cluster_name
+  cbr_enforcement_mode = var.cbr_enforcement_mode
+  cos                  = ibm_resource_instance.cos
+  keyprotect           = ibm_resource_instance.keyprotect
+  homezone_iprange     = var.homezone_iprange
 }
