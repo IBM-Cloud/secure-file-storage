9a56bcc5ca6ef6265e05431ceaffdc0a8030d50c
Henrik Loeser <hloeser@de.ibm.com>
2023-01-16 13:14:01 +0100

    simplification

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -50,9 +50,9 @@ variable "kp_plan" {
 
 # variables for the toolchain
 
-variable "IC_SCHEMATICS_WORKSPACE_ID" {
-  description = "ID of this Schematics workspace, filled automatically, LEAVE EMPTY"
-}
+#variable "IC_SCHEMATICS_WORKSPACE_ID" {
+#  description = "ID of this Schematics workspace, filled automatically, LEAVE EMPTY"
+#}
 
 variable "toolchain_registry_namespace" {
   description = "namespace in the Container Registry to store the container image"
