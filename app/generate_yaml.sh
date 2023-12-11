dcdee99085f4449a85346cb50eb8ebfa8575a60d
Henrik Loeser <hloeser@de.ibm.com>
2023-12-11 15:16:40 +0100

    switch to OIDC and passport

--- app/generate_yaml.sh
+++ app/generate_yaml.sh
@@ -26,13 +26,9 @@ if [[ -z "$TARGET_NAMESPACE" ]]; then
   export TARGET_NAMESPACE=default
 fi
 
-if [[ -z "$APPID_INSTANCE" ]]; then
-  export APPID_INSTANCE=sfsappid
-fi
-
 if [[ -z "$PUBLIC_CERT_ID" ]] && [[ -z "$SECRETS_MANAGER_API_URL" ]] && [[ -z "$MYDOMAIN" ]]; then
   cat secure-file-storage.template.yaml | \
-    envsubst '$APPID_INSTANCE $IMAGE_NAME $INGRESS_SECRET $INGRESS_SUBDOMAIN $IMAGE_PULL_SECRET $IMAGE_REPOSITORY $TARGET_NAMESPACE $BASENAME' > secure-file-storage.yaml
+    envsubst '$IMAGE_NAME $INGRESS_SECRET $INGRESS_SUBDOMAIN $IMAGE_PULL_SECRET $IMAGE_REPOSITORY $TARGET_NAMESPACE $BASENAME' > secure-file-storage.yaml
   exit
 fi
 
@@ -53,4 +49,4 @@ fi
 
 cat secure-file-storage.template.yaml | \
   sed -e 's/^# //' |
-  envsubst '$APPID_INSTANCE $PUBLIC_CERT_ID $SECRETS_MANAGER_API_URL $MYDOMAIN $IMAGE_NAME $INGRESS_SECRET $INGRESS_SUBDOMAIN $IMAGE_PULL_SECRET $IMAGE_REPOSITORY $TARGET_NAMESPACE $BASENAME' > secure-file-storage.yaml
+  envsubst '$PUBLIC_CERT_ID $SECRETS_MANAGER_API_URL $MYDOMAIN $IMAGE_NAME $INGRESS_SECRET $INGRESS_SUBDOMAIN $IMAGE_PULL_SECRET $IMAGE_REPOSITORY $TARGET_NAMESPACE $BASENAME' > secure-file-storage.yaml
